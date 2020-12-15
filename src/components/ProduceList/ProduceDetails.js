0cc50a27d733c35e25d217e9dd5262cd0139ccee
Derek Nungesser <nungesserfilms@gmail.com>
2020-12-15 12:33:50 -0500

    first

---
+++ src/components/ProduceList/ProduceDetails.js
@@ -0,0 +1,23 @@
+function ProduceDetails({ produce }) {
+  const cartItem = {}; //Placeholder for Redux access
+
+  return (
+    <li className="produce-details">
+      <span>{produce.name}</span>
+      <span>
+        <button
+          className={"like-button" + (produce.liked ? " selected" : "")}
+        >
+          <i className={"fas fa-heart"} />
+        </button>
+        <button
+          className={"plus-button" + (cartItem ? " selected" : "")}
+        >
+          <i className="fas fa-plus" />
+        </button>
+      </span>
+    </li>
+  );
+}
+
+export default ProduceDetails;
