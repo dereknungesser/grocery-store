5bf361e3995578ecaa46d365b45119261fe8b36e
Derek Nungesser <nungesserfilms@gmail.com>
2020-12-15 13:12:17 -0500

    populating produce

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import configureStore from './store/index'
-import { Provider } from 'react-redux'
+import configureStore from './store/index';
+import { Provider } from 'react-redux';
+import { populateProduce } from './store/produce';
 
 const store = configureStore();
+
+if (process.env.NODE_ENV !== "production") {
+  window.store = store;
+  window.populateProduce = populateProduce;
+}
+
 
 function Root() {
   return (
